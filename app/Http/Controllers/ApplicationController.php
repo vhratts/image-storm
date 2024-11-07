64d2cc1e8e5a15f65f42feb18a5352ddfd775511
vhratts <118541950+vratts@users.noreply.github.com>
2024-11-07 17:23:29 -0300

    Atualização do projeto: ✅ Atualização do soporte a Vercel #5 (TESTE)

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 use Illuminate\View\View;
 
 class ApplicationController extends Controller
 {
-    public function ApplicationHomePage(Request $request): View{
-        return view("app");
+    public function ApiAssetController($any = '/', Request $request)
+    {
+        if(!Str::contains($any, '.')){
+            return response()->json([
+                "status" => false,
+                "message" => "URI is not file"
+            ], 404); 
+        }
+
+        $filePath = public_path($any);
+
+        // Verifica se o arquivo existe
+        if (File::exists($filePath)) {
+            // Retorna o arquivo com o tipo correto (mime type)
+            return Response::file($filePath);
+        }
+
+        return abort(404);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\ApplicationController;
 use App\Http\Controllers\ImageEditorController;
 use App\Livewire\Pages\ApplicationHome;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", ApplicationHome::class)->name("Application-home");
+Route::get('/assets/{any}', [ApplicationController::class, 'ApiAssetController'])->where('any', '.*');;
 Route::prefix("/api")->group(function(){
     Route::post('/image-editor', [ImageEditorController::class, 'editImage']);
 });
