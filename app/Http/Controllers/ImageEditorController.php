2be7e10601eec840584327b26a0f36d3d43f4d70
vhratts <118541950+vratts@users.noreply.github.com>
2024-11-07 18:33:52 -0300

    Atualização do projeto: ✅ #5 - COrreção de bugs

--- app/Http/Controllers/ImageEditorController.php
+++ app/Http/Controllers/ImageEditorController.php
@@ -134,4 +134,29 @@ class ImageEditorController extends Controller
             'b' => hexdec(substr($hex, 4, 2))
         ];
     }
+
+    public function MimeTest()
+    {
+        // Cria a imagem com GD
+        $handle = ImageCreateTrueColor(130, 50) or die("Não foi possível criar a imagem");
+
+        // Define as cores
+        $bgColor = ImageColorAllocate($handle, 255, 0, 0); // Fundo vermelho
+        $txtColor = ImageColorAllocate($handle, 255, 255, 255); // Texto branco
+        $lineColor = ImageColorAllocate($handle, 0, 0, 0); // Linha preta
+
+        // Preenche o fundo e desenha uma linha e o texto
+        ImageFill($handle, 0, 0, $bgColor);
+        ImageLine($handle, 65, 0, 130, 50, $lineColor);
+        ImageString($handle, 5, 5, 18, "->Image-Storm", $txtColor);
+
+        // Armazena a imagem em um buffer de saída
+        ob_start();
+        ImagePng($handle);
+        $imageData = ob_get_clean();
+        ImageDestroy($handle);
+
+        // Retorna a imagem com o cabeçalho correto
+        return response($imageData, 200)->header('Content-Type', 'image/png');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,6 @@ use Illuminate\Support\Facades\Route;
 Route::get("/", ApplicationHome::class)->name("Application-home");
 Route::get('/assets/{any}', [ApplicationController::class, 'ApiAssetController'])->where('any', '.*');;
 Route::prefix("/api-service")->group(function(){
+    Route::any('/', [ImageEditorController::class, 'MimeTest']);
     Route::post('/image-editor', [ImageEditorController::class, 'editImage']);
 });
